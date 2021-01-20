e77d14d819741a83d536766d4cada988015cd8b4
harens <harensdeveloper@gmail.com>
2021-01-20 12:48:24 +0000

    beartype 0.5.1 (new formula)

---
+++ Formula/beartype.rb
@@ -0,0 +1,30 @@
+class Beartype < Formula
+    include Language::Python::Virtualenv
+  
+    desc "Unbearably fast O(1) runtime type-checking in pure Python"
+    homepage "https://github.com/beartype/beartype"
+    url "https://files.pythonhosted.org/packages/19/d5/4f6d70a2096ce24ad91b8fabdac28643fdddf920e0f7977cde7bd1bb7296/beartype-0.5.1.tar.gz"
+    sha256 "195b1ea1834511b876507563808d8ca602d7cfb141ab9660c17a5148fb38eeb9"
+    license "MIT"
+    # Default branch is "main" not "master" (unbearably modern)
+    head "https://github.com/beartype/beartype.git", branch: "main"
+  
+    livecheck do
+      url :stable
+    end
+  
+    depends_on "python@3.9"
+  
+    def install
+      system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(prefix)
+    end
+  
+    test do
+      # Simple version number check
+      system Formula["python@3.9"].opt_bin/"python3", "-c", <<~EOS
+        import #{name}
+        assert #{name}.__version__ == "#{version}"
+      EOS
+    end
+  end
+  
