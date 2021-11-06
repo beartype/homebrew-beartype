7c1659b19afa45261c66714a06f9a22dbe91ec80
Haren S <harens@macports.org>
2021-11-06 10:06:01 +0000

    beartype 0.9.1

--- Formula/beartype.rb
+++ Formula/beartype.rb
@@ -3,8 +3,8 @@ class Beartype < Formula
 
   desc "Unbearably fast O(1) runtime type-checking in pure Python"
   homepage "https://github.com/beartype/beartype"
-  url "https://files.pythonhosted.org/packages/6b/c7/a73fbaa2f2abc1120e6d16cfb89cdb6f7ea3ba8e715dbc324ac1aafe7b4b/beartype-0.9.0.tar.gz"
-  sha256 "a94691716c246a25a56e8a23942796b3133b5f444d92c1620329b81671be1311"
+  url "https://files.pythonhosted.org/packages/ef/16/11a1509d5cd7f3760101edda30f66b28367f2793178bea57745733e92076/beartype-0.9.1.tar.gz"
+  sha256 "623630dc243b0da5a84f0fa414e6aaaf2613e567ad19abfe6a81c17ea5ab62f1"
   license "MIT"
   # Default branch is "main" not "master" (unbearably modern)
   head "https://github.com/beartype/beartype.git", branch: "main"
