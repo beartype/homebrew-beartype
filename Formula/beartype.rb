5d7167c32041335aadc39628520843b378372c89
harens <harensdeveloper@gmail.com>
2021-11-06 10:11:52 +0000

    beartype: migrate to python@3.10

--- Formula/beartype.rb
+++ Formula/beartype.rb
@@ -15,14 +15,14 @@ class Beartype < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "7752d46c877e1c97b8f5713844b1076cf5416f08f3e8bd5729c1ca06ccf1bb60"
   end
 
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   def install
     # Based on name-that-hash
     # https://github.com/Homebrew/homebrew-core/blob/9652b75b2bbaf728f70c50b09cce39520c08321d/Formula/name-that-hash.rb
     virtualenv_install_with_resources
 
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    xy = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
     site_packages = "lib/python#{xy}/site-packages"
     pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
     (prefix/site_packages/"homebrew-beartype.pth").write pth_contents
@@ -30,7 +30,7 @@ class Beartype < Formula
 
   test do
     # Simple version number check
-    system Formula["python@3.9"].opt_bin/"python3", "-c", <<~EOS
+    system Formula["python@3.10"].opt_bin/"python3", "-c", <<~EOS
       import #{name}
       assert #{name}.__version__ == "#{version}"
     EOS
