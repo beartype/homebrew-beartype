fe7655d51a84005e9b5158c488e19770ea61ad99
Haren S <harens@users.noreply.github.com>
2023-01-17 17:30:11 +0000

    beartype 0.12.0
    * beartype 0.12.0
    * beartype: migrate to python@3.11
    
    Closes #18.
    
    Co-authored-by: harens <12570877+harens@users.noreply.github.com>
    Signed-off-by: Haren S <12570877+harens@users.noreply.github.com>

--- Formula/beartype.rb
+++ Formula/beartype.rb
@@ -3,8 +3,8 @@ class Beartype < Formula
 
   desc "Unbearably fast O(1) runtime type-checking in pure Python"
   homepage "https://github.com/beartype/beartype"
-  url "https://files.pythonhosted.org/packages/b3/7c/0c6451ff5477cf6f3ac5101cafd084e43e3a29242d28f2056cad76ff03e9/beartype-0.11.0.tar.gz"
-  sha256 "3854b50eaaa98bb89490be57e73c69c777a0f304574e7043ac7da98ac6a735a6"
+  url "https://files.pythonhosted.org/packages/95/78/bf7a20712dbf3a18e37f9ae1c2a1e04169c43b4628871f40f1db5f29c257/beartype-0.12.0.tar.gz"
+  sha256 "3b7545b3f333a6b07042b68b102141554c9add2e979dab7b0f8ed6378f7af7d7"
   license "MIT"
   # Default branch is "main" not "master" (unbearably modern)
   head "https://github.com/beartype/beartype.git", branch: "main"
@@ -16,14 +16,14 @@ class Beartype < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "e6f86201d690fed990d41a9111ba6d5bfcb0c615a68f7635c5761773fab33a1f"
   end
 
-  depends_on "python@3.10"
+  depends_on "python@3.11"
 
   def install
     # Based on name-that-hash
     # https://github.com/Homebrew/homebrew-core/blob/9652b75b2bbaf728f70c50b09cce39520c08321d/Formula/name-that-hash.rb
     virtualenv_install_with_resources
 
-    xy = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
+    xy = Language::Python.major_minor_version Formula["python@3.11"].opt_bin/"python3"
     site_packages = "lib/python#{xy}/site-packages"
     pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
     (prefix/site_packages/"homebrew-beartype.pth").write pth_contents
@@ -31,7 +31,7 @@ class Beartype < Formula
 
   test do
     # Simple version number check
-    system Formula["python@3.10"].opt_bin/"python3", "-c", <<~EOS
+    system Formula["python@3.11"].opt_bin/"python3.11", "-c", <<~EOS
       import #{name}
       assert #{name}.__version__ == "#{version}"
     EOS
